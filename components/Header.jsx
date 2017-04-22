18d9bcf6415229c4deb15de0b686085e54e53e28
pbialy <pbialy@pbialy.eu>
2017-04-23 01:46:00 +0200

    from react tries

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import ValueField from './ValueField.jsx'; //bez jsx?
 import EditField from './EditField.jsx'; //bez jsx?
 
 class Header extends React.Component {
-
+/*
     constructor(props) {
         super(props);
         this.state = {
@@ -24,14 +24,41 @@ class Header extends React.Component {
     updateState(e) {
         //debugger;
         //this.setState({ data: e.target.value });
-        //this.setState(Object.assign({}, this.state, e));
+        this.setState(Object.assign({}, this.state, e));
     };
 
     changeMode(){
         console.log(`changeMode editMode=${this.state.editMode}`);
         this.setState(Object.assign({}, this.state, {editMode: !this.state.editMode}));
     }
+*/
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: 'initial data'
+        };
+        this.updateState = this.updateState.bind(this)
+    };
+
+    updateState(e) {
+		debugger;
+        this.setState({ data: e.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                <EditField data={this.state.data} updateState={this.updateState} />
+            </div>
+        );
+
+    };
+
+};
+
+
+/*
     render() {
         const myStyle = {
             backgroundColor: 'yellow',
@@ -58,5 +85,5 @@ class Header extends React.Component {
         );
     }
 }
-
+*/
 export default Header;
